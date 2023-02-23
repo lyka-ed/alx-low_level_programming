2aff3938122f43511f336590c1b402a901e854e2
Glory-Lyka Okposin <lykaedem11@gmail.com>
2023-02-23 01:35:28 -0800

    Nested Loop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two integers
+ * @a: Input integer
+ * @b: Input integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
