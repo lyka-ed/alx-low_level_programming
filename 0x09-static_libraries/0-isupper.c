17f989f002e4fcb5627d52cb6a99ae129725ae29
Glory-Lyka Okposin <lykaedem11@gmail.com>
2023-03-10 01:34:48 -0800

    libmy

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Function that checks for uppercase character
+ * @c: input variable
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+        char uppercase = 'A';
+        int isupper = 0;
+
+        for (; uppercase <= 'Z'; uppercase++)
+        {
+                if (c == uppercase)
+                {
+                        isupper = 1;
+                        break;
+                }
+        }
+
+        return (isupper);
+}
